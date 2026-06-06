5dcd2550f62a93937247686665fc2b0cb0345c81
YuanPing <yangderui.ydr@antgroup.com>
2026-06-06 16:48:03 +0800

    refactor(domain): 阶段1 Domain 模型变更
    - 从 Account 结构体中删除 opened_at 字段
    - 将 Transaction.date 改名为 Transaction.date_time，类型改为 NaiveDateTime

--- accounting/src/account.rs
+++ accounting/src/account.rs
@@ -13,8 +13,6 @@ pub struct Account {
     pub account_type: AccountType,
     /// 父账户 ID，根账户为 None
     pub parent_id: Option<AccountId>,
-    /// 开户日期
-    pub opened_at: NaiveDate,
     /// 关户日期，未关闭为 None
     pub closed_at: Option<NaiveDate>,
     /// 是否为系统内置账户
@@ -30,8 +28,6 @@ mod tests {
     use super::*;
     use crate::account_type::AccountType;
     use crate::id::AccountId;
-    use chrono::NaiveDate;
-
     #[test]
     fn test_account_fields() {
         let a = Account {
@@ -39,7 +35,6 @@ mod tests {
             full_name: "Assets:Cash".to_string(),
             account_type: AccountType::Asset,
             parent_id: None,
-            opened_at: NaiveDate::from_ymd_opt(2024, 1, 1).unwrap(),
             closed_at: None,
             is_system: false,
             billing_day: None,
--- accounting/src/transaction.rs
+++ accounting/src/transaction.rs
@@ -1,13 +1,13 @@
 use crate::id::{MemberId, TransactionId};
-use chrono::NaiveDate;
+use chrono::NaiveDateTime;
 
 /// 交易
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Transaction {
     /// 交易唯一标识符
     pub id: TransactionId,
-    /// 交易日期
-    pub date: NaiveDate,
+    /// 交易时间
+    pub date_time: NaiveDateTime,
     /// 交易描述
     pub description: String,
     /// 关联成员 ID
